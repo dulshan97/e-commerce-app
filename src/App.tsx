ab7bc1ad9636d941f65f09cda908a073d684e9d4
dulshan_97 <dulshanweerasinghe@gmail.com>
2024-12-03 19:54:37 +0530

    pasing props search and filtering

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,24 @@ import ShoppingCart from './components/Cart/shoppingCart';
 import NavBar from './components/common/navbar';
 import { RouteName } from './routes/RouteName';
 import { CartProvider } from './context/cartContext';
+import { useState } from 'react';
 
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
   return (
     <CartProvider>
       <Router>
         <Routes>
-          <Route element={< NavBar />}>
-            <Route path={RouteName.ROOT} element={<ProductList />} />
+          <Route element={< NavBar
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />}>
+            <Route path={RouteName.ROOT} element={<ProductList searchTerm={searchTerm} selectedCategory={selectedCategory} />} />
             <Route path={RouteName.LOGIN} element={<Login />} />
             <Route path={RouteName.SIGNUP} element={<Signup />} />
             <Route path={RouteName.CART} element={<ShoppingCart />} />
