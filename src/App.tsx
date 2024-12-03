ae78d4b68fe38417e7c6d5252e571e44f2b88d86
dulshan_97 <dulshanweerasinghe@gmail.com>
2024-12-03 18:54:50 +0530

    wrap with card provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import Signup from './components/Auth/signup';
 import ShoppingCart from './components/Cart/shoppingCart';
 import NavBar from './components/common/navbar';
 import { RouteName } from './routes/RouteName';
+import { CartProvider } from './context/cartContext';
 
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route element={< NavBar />}>
-          <Route path={RouteName.ROOT} element={<ProductList />} />
-          <Route path={RouteName.LOGIN} element={<Login />} />
-          <Route path={RouteName.SIGNUP} element={<Signup />} />
-          <Route path={RouteName.CART} element={<ShoppingCart />} />
-        </Route>
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route element={< NavBar />}>
+            <Route path={RouteName.ROOT} element={<ProductList />} />
+            <Route path={RouteName.LOGIN} element={<Login />} />
+            <Route path={RouteName.SIGNUP} element={<Signup />} />
+            <Route path={RouteName.CART} element={<ShoppingCart />} />
+          </Route>
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 };
 
