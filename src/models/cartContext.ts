d9d2b15ea3916dc758a85c9d8295ed6374b2a699
dulshan_97 <dulshanweerasinghe@gmail.com>
2024-12-03 19:14:08 +0530

    cart context model

---
+++ src/models/cartContext.ts
@@ -0,0 +1,9 @@
+import { CartItem, Product } from "./product";
+
+export interface CartContextType {
+    cart: CartItem[];
+    addToCart: (product: Product) => void;
+    removeFromCart: (productId: string) => void;
+    updateQuantity: (productId: string, quantity: number) => void;
+    cartTotal: number;
+  }
